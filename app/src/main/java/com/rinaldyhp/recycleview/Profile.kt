0c691b8a6b3cfafed80c10c165a485e39a99ee60
rinaldyhp <rinaldyhudanp@gmail.com>
2022-07-03 15:33:18 +0700

    Recycleview App

---
+++ app/src/main/java/com/rinaldyhp/recycleview/Profile.kt
@@ -0,0 +1,22 @@
+package com.rinaldyhp.recycleview
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rinaldyhp.recycleview.databinding.ActivityProfileBinding
+
+class Profile : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnback.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
